d69544f193d4540f78b5fd35ebe6ab65b1bdfdf1
R00dRallec <mail@c-becker.com>
2016-01-27 20:27:01 +0100

    Create root_vim_install.sh

---
+++ scripts/root_vim_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# os installs
+apt-get update
+apt-get install vim -y
+
+# install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Configure vimrc
+#wget https://raw.githubusercontent.com/R00dRallec/stuff/master/settings/vimrc ~/.vimrc
+mv ~/.vimrc ~/.vimrc.bak
+cp ../settings/vimrc ~/.vimrc
+
+# install powerline roboto font
+git clone https://github.com/powerline/fonts.git ~/fonts
+font_dir="$HOME/.local/share/fonts"
+mkdir -p $font_dir
+cp fonts/RobotoMono/* $font_dir
+
+# Reset font cache on Linux
+if command -v fc-cache @>/dev/null ; then
+    echo "Resetting font cache, this may take a moment..."
+    fc-cache -f $font_dir
+fi
+
+echo "Roboto Powerline font installed to $font_dir"
+
+# clean up
+rm -rf ~/fonts
+
+vim +PluginInstall +qall
+
+echo "Done"
