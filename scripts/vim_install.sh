1647ee316018555fcc31b0a87745fb57cd44eb83
R00dRallec <mail@c-becker.com>
2016-01-27 14:33:09 +0100

    Added roboto powerline font installation

--- scripts/vim_install.sh
+++ scripts/vim_install.sh
@@ -12,6 +12,23 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mv ~/.vimrc ~/.vimrc.bak
 cp ../settings/vimrc ~/.vimrc
 
+# install powerline roboto font
+git clone https://github.com/powerline/fonts.git ~/fonts
+font_dir="$HOME/.local/share/fonts"
+mkdir -p $font_dir
+cp fonts/RobotoMono/* $font_dir
+
+# Reset font cache on Linux
+if command -v fc-cache @>/dev/null ; then
+    echo "Resetting font cache, this may take a moment..."
+    fc-cache -f $font_dir
+fi
+
+echo "Roboto Powerline font installed to $font_dir"
+
+# clean up
+rm -rf ~/fonts
+
 vim +PluginInstall +qall
 
 echo "Done"
