2914e0302bb1ab3a6b193ea9fb43fba93cfdf6ab
R00dRallec <mail@c-becker.com>
2016-01-27 20:53:55 +0100

    Update postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -10,41 +10,30 @@ apt-get -y install vim git
 apt-get -y install nmon ncdu dstat ccze
 # Description see: http://www.unixmen.com/10-useful-utilities-linux-users
 
-# folders
-rm -rf ~/Documents
-rm -rf ~/Public
-rm -rf ~/Templates
-rm -rf ~/Videos
-rm -rf ~/Music
-rm ~/examples.desktop
-mkdir -p ~/Development
-
-# users
-
+# user
 pass=$(perl -e 'print crypt($ARGV[0], "password")' dev)
 useradd -m -p $pass 'dev'
 
 # user specific settings
-su dev
 
 # bash settings
-echo "alias ll='ls -l'" >> ~/.bashrc
-echo "alias la='ls -a'" >> ~/.bashrc
-echo "alias lla='ls -la'" >> ~/.bashrc
+su -m dev -c echo "alias ll='ls -l'" >> ~/.bashrc
+su -m dev -c echo "alias la='ls -a'" >> ~/.bashrc
+su -m dev -c echo "alias lla='ls -la'" >> ~/.bashrc
 
 # vim coloring
-git clone git://github.com/altercation/vim-colors-solarized.git ~/cloned-bundle
-cp -r ~/cloned-bundle/* ~/.vim/bundle
-echo "syntax enable" >> ~/.vimrc
-echo "set background=dark" >> ~/.vimrc
-echo "colorscheme solarized" >> ~/.vimrc
+su -m dev -c git clone git://github.com/altercation/vim-colors-solarized.git ~/cloned-bundle
+su -m dev -c cp -r ~/cloned-bundle/* ~/.vim/bundle
+su -m dev -c echo "syntax enable" >> ~/.vimrc
+su -m dev -c echo "set background=dark" >> ~/.vimrc
+su -m dev -c echo "colorscheme solarized" >> ~/.vimrc
 
 # invoke vim_install.sh
-sh vim_install.sh
+su -m dev -csh vim_install.sh
 
 # Clean up
-rm -rf ~/cloned-bundle
-rm -rf ~/gnome-terminal-colors-solarized
+su -m dev -c rm -rf ~/cloned-bundle
+su -m dev -c rm -rf ~/gnome-terminal-colors-solarized
 
 # prompt for a reboot
 clear
